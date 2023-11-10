58ccd10ac9c40d99da2ffb386ba297417c7590f3
Ivania <ivania.velis@catolica.edu.sv>
2023-11-10 11:09:39 -0600

    Menu datos

--- app/src/main/java/com/example/agenda_panaderia/Menu_Principal.java
+++ app/src/main/java/com/example/agenda_panaderia/Menu_Principal.java
@@ -1,8 +1,9 @@
 package com.example.agenda_panaderia;
 
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,7 +13,6 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,89 +23,112 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Menu_Principal extends AppCompatActivity {
 
-    Button Btn_Salir;
-    FirebaseAuth firebaseAuth;
-    FirebaseUser User;
-    TextView NombreP,Linear_Correo;
-    ProgressBar ProgresBar;
-    DatabaseReference Usuarios;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_principal);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("OrderEase");
-        NombreP=findViewById(R.id.NombreP);
-        Linear_Correo= findViewById(R.id.CorreoP);
-        ProgresBar= findViewById(R.id.ProgresBar);
-
-        Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
-        Btn_Salir=findViewById(R.id.Btn_Salir);
-        firebaseAuth = FirebaseAuth.getInstance();
-        User= firebaseAuth.getCurrentUser();
-
-        Btn_Salir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {  SalirAplicacion();
-
-            }
-        });
 
 
-    }
-    @Override
-    protected void onStart() {
-        ComprobarInicioSesion();
-        super.onStart();
-    }
 
-    private void ComprobarInicioSesion(){
-        if (User!=null){
-            //El usuario ha iniciado sesión
-            CargaDeDatos();
-        }else {
-            //Lo dirigirá al MainActivity
-            startActivity(new Intent(Menu_Principal.this,MainActivity.class));
-            finish();
-        }
-    }
+        Button Btn_Salir;
+        FirebaseAuth firebaseAuth;
+        FirebaseUser User;
+        LinearLayoutCompat Linear_Nombres, Linear_Verificacion, Linear_Correo;
+        TextView NombreP,Linear, IdMenu, Verificacion;
+        ProgressBar ProgresBar;
+        DatabaseReference Usuarios;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_menu_principal);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setTitle("OrderEase");
+            NombreP=findViewById(R.id.NombreP);
+            Linear= findViewById(R.id.CorreoP);
 
-    private void CargaDeDatos(){
-        Usuarios.child(User.getUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                //Si el usuario existe
-                if (snapshot.exists()){
-                    //El progressbar se oculta
-                    ProgresBar.setVisibility(View.GONE);
-                    //Los TextView se muestran
-                    NombreP.setVisibility(View.VISIBLE);
-                    Linear_Correo.setVisibility(View.VISIBLE);
+            IdMenu= findViewById(R.id.IdMenu);
+            Linear_Nombres = findViewById(R.id.Linear_Nombres);
+            Linear_Verificacion = findViewById(R.id.Linear_Verificacion);
+            Linear_Correo = findViewById(R.id.Linear_Correo);
+            Verificacion = findViewById(R.id.Verificacion);
 
-                    //Obtener los datos
-                    String nombre = ""+snapshot.child("nombre").getValue();
-                    String correo = ""+snapshot.child("correo").getValue();
 
-                    //Setear los datos en los respectivos TextView
-                    NombreP.setText(nombre);
-                    Linear_Correo.setText(correo);
+
+
+            ProgresBar= findViewById(R.id.ProgresBar);
+
+            Usuarios = FirebaseDatabase.getInstance().getReference("Usuarios");
+            Btn_Salir=findViewById(R.id.Btn_Salir);
+            firebaseAuth = FirebaseAuth.getInstance();
+            User= firebaseAuth.getCurrentUser();
+
+            Btn_Salir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {  SalirAplicacion();
 
                 }
+            });
+
+
+        }
+        @Override
+        protected void onStart() {
+            ComprobarInicioSesion();
+            super.onStart();
+        }
+
+        private void ComprobarInicioSesion(){
+            if (User!=null){
+                //El usuario ha iniciado sesión
+                CargaDeDatos();
+            }else {
+                //Lo dirigirá al MainActivity
+                startActivity(new Intent(Menu_Principal.this,MainActivity.class));
+                finish();
             }
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        private void CargaDeDatos(){
+            Usuarios.child(User.getUid()).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-            }
-        });
+                    //Si el usuario existe
+                    if (snapshot.exists()){
+                        //El progressbar se oculta
+                        ProgresBar.setVisibility(View.GONE);
+                        //Los TextView se muestran
+                        NombreP.setVisibility(View.VISIBLE);
+                        Linear.setVisibility(View.VISIBLE);
+
+
+                        IdMenu.setVisibility(View.VISIBLE);
+                        Linear_Nombres.setVisibility(View.VISIBLE);
+                        Linear_Verificacion.setVisibility(View.VISIBLE);
+                        Linear_Correo.setVisibility(View.VISIBLE);
+                        Verificacion.setVisibility(View.VISIBLE);
+
+
+
+                        //Obtener los datos
+                        String nombre = ""+snapshot.child("nombre").getValue();
+                        String correo = ""+snapshot.child("correo").getValue();
+
+                        //Setear los datos en los respectivos TextView
+                        NombreP.setText(nombre);
+                        Linear.setText(correo);
+
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        private void SalirAplicacion() {
+            firebaseAuth.signOut();
+            startActivity(new Intent(Menu_Principal.this, MainActivity.class));
+            Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
+        }
     }
-
-    private void SalirAplicacion() {
-        firebaseAuth.signOut();
-        startActivity(new Intent(Menu_Principal.this, MainActivity.class));
-        Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
-    }
-}
-
-
