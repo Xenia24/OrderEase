353bac7e71cb9685f91b739c35ba09bdd8da47ab
Ivania <ivania.velis@catolica.edu.sv>
2023-11-13 10:10:48 -0600

    mostrar contacto

--- app/src/main/java/com/example/agenda_panaderia/Contactos/Listar_Contactos.java
+++ app/src/main/java/com/example/agenda_panaderia/Contactos/Listar_Contactos.java
@@ -1,26 +1,48 @@
 package com.example.agenda_panaderia.Contactos;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agenda_panaderia.Menu_Principal;
+import com.example.agenda_panaderia.Objetos.Contacto;
 import com.example.agenda_panaderia.R;
+import com.example.agenda_panaderia.ViewHolder.ViewHolderContacto;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 
 public class Listar_Contactos extends AppCompatActivity {
     ImageView ImageView, atras;
+    RecyclerView recyclerViewContactos;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference BD_Usuarios;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseRecyclerAdapter <Contacto, ViewHolderContacto> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Contacto> firebaseRecyclerOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +50,18 @@ public class Listar_Contactos extends AppCompatActivity {
         setContentView(R.layout.activity_listar_contactos);
         ImageView = findViewById(R.id.Agregar_Contacto);
         atras = findViewById(R.id.regresar);
+
+        recyclerViewContactos = findViewById(R.id.recyclerViewContactos);
+        recyclerViewContactos.setHasFixedSize(true);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        BD_Usuarios = firebaseDatabase.getReference("Usuarios");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
+
+
         ImageView.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -39,7 +73,7 @@ public class Listar_Contactos extends AppCompatActivity {
 
                 //Toast.makeText(Menu_Principal.this,"Contactos", Toast.LENGTH_SHORT).show();
             }
-            });
+        });
 
         atras.setOnClickListener(new View.OnClickListener() {
 
@@ -51,8 +85,62 @@ public class Listar_Contactos extends AppCompatActivity {
                 //uno dos tres
             }
         });
-
-
+        ListarContactos();
     }
 
+    private void ListarContactos() {
+        Query query = BD_Usuarios.child(user.getUid()).child("Contactos").orderByChild("nombres");
+
+        firebaseRecyclerOptions = new FirebaseRecyclerOptions.Builder<Contacto>().setQuery(query, Contacto.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Contacto, ViewHolderContacto>(firebaseRecyclerOptions) {
+
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolderContacto viewHolderContacto, int position, @NonNull Contacto contacto) {
+                viewHolderContacto.SetearDatosContacto(
+                        getApplicationContext(),
+                        contacto.getId_contacto(),
+                        contacto.getUid_contacto(),
+                        contacto.getNombres(),
+                        contacto.getApellidos(),
+                        contacto.getCorreo(),
+                        contacto.getTelefono(),
+                        contacto.getDireccion()
+                );
+
+            }
+
+            @NonNull
+            @Override
+            public ViewHolderContacto onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contacto, parent, false);
+                ViewHolderContacto viewHolderContacto = new ViewHolderContacto(view);
+                viewHolderContacto.setOnClickListener(new ViewHolderContacto.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Toast.makeText(Listar_Contactos.this, "On item click", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(Listar_Contactos.this, "On item long click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                return viewHolderContacto;
+
+            }
+
+        };
+        recyclerViewContactos.setLayoutManager(new GridLayoutManager(Listar_Contactos.this, 2));
+        {
+            firebaseRecyclerAdapter.startListening();
+            recyclerViewContactos.setAdapter(firebaseRecyclerAdapter);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseRecyclerAdapter != null) {
+            firebaseRecyclerAdapter.startListening();
+        }
+    }
 }
--- app/src/main/java/com/example/agenda_panaderia/ViewHolder/ViewHolderContacto.java
+++ app/src/main/java/com/example/agenda_panaderia/ViewHolder/ViewHolderContacto.java
@@ -9,19 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agenda_panaderia.R;
 
-import kotlin.text.UStringsKt;
-
 public class ViewHolderContacto extends RecyclerView.ViewHolder {
     View mView;
 
     private  ViewHolderContacto.ClickListener mClickListener;
-public interface ClickListener {
-    void onItemClick(View view, int position);/*Se ejecuta al presionar en el item*/
-    void onItemLongClick(View view, int position);/*Se ejecuta al mantener presionado en el item*/
-}
-public void  setOnItemLongClickListener(ViewHolderContacto.ClickListener clickListener){
-    mClickListener = clickListener;
-}
+
+
+
+    public interface ClickListener {
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+    public void setOnClickListener(ViewHolderContacto.ClickListener clickListener) {
+        mClickListener = clickListener;
+    }
 
     public ViewHolderContacto(@NonNull View itemView) {
         super(itemView);
@@ -31,36 +32,46 @@ public void  setOnItemLongClickListener(ViewHolderContacto.ClickListener clickLi
 
             @Override
             public void onClick(View view) {
-                mClickListener.onItemClick(view,getAdapterPosition());
+                mClickListener.onItemClick(view,getBindingAdapterPosition());
             }
         });
 
-        itemView.setOnClickListener(new View.OnClickListener(){
+        itemView.setOnLongClickListener(new View.OnLongClickListener(){
 
             @Override
-            public void onClick(View view) {
-                mClickListener.onItemLongClick(view,getAdapterPosition());
+            public boolean onLongClick(View view) {
+                mClickListener.onItemLongClick(view,getBindingAdapterPosition());
+                return false;
             }
         });
     }
-    public void SetearDatosContacto(Context context, String id_contacto, String uid_contacto, String nombres, String apellidos, String correo, String telefono, String direccion){
+    public void SetearDatosContacto(Context context,
+                                    String id_contacto,
+                                    String uid_contacto,
+                                    String nombres,
+                                    String apellidos,
+                                    String correo,
+                                    String telefono,
+                                    String direccion){
 
-        TextView Uid_Usuario, Nombre_c,Apellido_c,Correo_c,Telefono_c,Direccion_c;
+        TextView Id_c_Item, Uid_c_Item,nombre_c_Item,apellido_c_Item,Correo_c_Item,telefono_c_Item,direccion_c_Item;
 
 
-        Uid_Usuario=mView.findViewById(R.id.Uid_Usuario);
-        Nombre_c=mView.findViewById(R.id.Nombre_c);
-        Apellido_c=mView.findViewById(R.id.Apellido_c);
-        Correo_c=mView.findViewById(R.id.Correo_c);
-        Telefono_c=mView.findViewById(R.id.Telefono_c);
-        Direccion_c=mView.findViewById(R.id.Direccion_c);
+        Id_c_Item=mView.findViewById(R.id.Id_c_Item);
+        Uid_c_Item=mView.findViewById(R.id.Uid_c_Item);
+        nombre_c_Item=mView.findViewById(R.id.nombre_c_Item);
+        apellido_c_Item=mView.findViewById(R.id.apellido_c_Item);
+        Correo_c_Item=mView.findViewById(R.id.Correo_c_Item);
+        telefono_c_Item=mView.findViewById(R.id.telefono_c_Item);
+        direccion_c_Item=mView.findViewById(R.id.direccion_c_Item);
 
-        Uid_Usuario.setText(uid_contacto);
-        Nombre_c.setText(nombres);
-        Apellido_c.setText(apellidos);
-        Correo_c.setText(correo);
-        Telefono_c.setText(telefono);
-        Direccion_c.setText(direccion);
+        Id_c_Item.setText(id_contacto);
+        Uid_c_Item.setText(uid_contacto);
+        nombre_c_Item.setText(nombres);
+        apellido_c_Item.setText(apellidos);
+        Correo_c_Item.setText(correo);
+        telefono_c_Item.setText(telefono);
+        direccion_c_Item.setText(direccion);
 
     }
 }
