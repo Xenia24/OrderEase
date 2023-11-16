471a54eae6e343600041e52889c1b950676c61ac
H-Franky <carlos.hurtado1@catolica.edu.sv>
2023-11-16 12:08:28 -0600

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/agenda_panaderia/AgregarPedido/Agregar_Pedidos.java
+++ app/src/main/java/com/example/agenda_panaderia/AgregarPedido/Agregar_Pedidos.java
@@ -169,7 +169,7 @@ public class Agregar_Pedidos extends AppCompatActivity {
                     descrip,
                     fecha,
                     formaEntrega);
-//h
+//j
 
             String nombre_BD = "Pedidos_Realizados";
             assert id_pedido != null;
