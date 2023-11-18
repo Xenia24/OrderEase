a9eceb7e2ccea56bcc3bb948f53f7d0547cac15b
ErickPineda12 <erick.pineda@catolica.edu.sv>
2023-11-17 21:08:58 -0600

    Contactos_Di

--- app/src/main/java/com/example/agenda_panaderia/Objetos/Pedido.java
+++ app/src/main/java/com/example/agenda_panaderia/Objetos/Pedido.java
@@ -2,9 +2,13 @@ package com.example.agenda_panaderia.Objetos;
 
 
     public class Pedido {
-        String id_pedido, uid_usuario,nombre, fecha_actual, titulo, descripcion,fecha_pedido,forma_entrega;
+        String id_pedido, uid_usuario,nombre, fecha_actual, titulo, descripcion,fecha_pedido,forma_entrega, estado;
 
-        public Pedido(String id_pedido, String fecha_actual, String uid_usuario, String nombre, String titulo, String descripcion, String fecha_pedido, String forma_entrega) {
+        public Pedido() {
+
+        }
+
+        public Pedido(String id_pedido, String fecha_actual,  String uid_usuario, String nombre, String titulo, String descripcion, String fecha_pedido, String forma_entrega, String estado) {
             this.id_pedido = id_pedido;
             this.uid_usuario = uid_usuario;
             this.nombre = nombre;
@@ -13,12 +17,18 @@ package com.example.agenda_panaderia.Objetos;
             this.descripcion = descripcion;
             this.fecha_pedido = fecha_pedido;
             this.forma_entrega = forma_entrega;
-
+            this.estado = estado;
 
         }
+
+
     public void setId_pedido(String id_pedido) {
         this.id_pedido = id_pedido;
     }
+
+    public String getId_pedido() {
+            return id_pedido;
+        }
 
     public String getUid_usuario() {
         return uid_usuario;
@@ -28,8 +38,6 @@ package com.example.agenda_panaderia.Objetos;
         this.uid_usuario = uid_usuario;
     }
 
-
-
     public String getFecha_actual() {
         return fecha_actual;
     }
@@ -37,7 +45,7 @@ package com.example.agenda_panaderia.Objetos;
     public void setFecha_actual(String fecha_actual) {
         this.fecha_actual = fecha_actual;
     }
-        public String getNombre() {
+    public String getNombre() {
             return nombre;
         }
 
@@ -69,7 +77,17 @@ package com.example.agenda_panaderia.Objetos;
         this.fecha_pedido = fecha_pedido;
     }
 
+    public String getEstado() {
+            return estado;
+        }
+
+    public void setEstado(String estado) {
+            this.estado = estado;
+        }
+
     public String getForma_entrega() {return forma_entrega;}
         public void setForma_entrega(String forma_entrega) {this.forma_entrega = forma_entrega;}
+
     }
+
 
