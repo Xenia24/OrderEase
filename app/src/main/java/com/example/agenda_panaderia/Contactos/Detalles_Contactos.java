02884af2cb45bf34583ca8fe5004a60d905e407a
ErickPineda12 <erick.pineda@catolica.edu.sv>
2023-11-19 19:38:13 -0600

    Diseño_DetalleC

---
+++ app/src/main/java/com/example/agenda_panaderia/Contactos/Detalles_Contactos.java
@@ -0,0 +1,16 @@
+package com.example.agenda_panaderia.Contactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_panaderia.R;
+
+public class Detalles_Contactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_contactos);
+    }
+}
