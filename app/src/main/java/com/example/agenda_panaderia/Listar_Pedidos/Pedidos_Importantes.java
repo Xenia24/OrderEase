f91150bb821e7e99e2501bb1f2eb398a0ef746c1
ErickPineda12 <erick.pineda@catolica.edu.sv>
2023-11-20 22:07:25 -0600

    Diseño_importantes

---
+++ app/src/main/java/com/example/agenda_panaderia/Listar_Pedidos/Pedidos_Importantes.java
@@ -0,0 +1,16 @@
+package com.example.agenda_panaderia.Listar_Pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_panaderia.R;
+
+public class Pedidos_Importantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedidos_importantes);
+    }
+}
