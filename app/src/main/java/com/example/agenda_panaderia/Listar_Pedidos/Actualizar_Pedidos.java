394bc016bb4a65e54ac2d49cf1561965515d0efb
ErickPineda12 <erick.pineda@catolica.edu.sv>
2023-11-20 23:55:30 -0600

    Diseño_actualizarP

---
+++ app/src/main/java/com/example/agenda_panaderia/Listar_Pedidos/Actualizar_Pedidos.java
@@ -0,0 +1,16 @@
+package com.example.agenda_panaderia.Listar_Pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_panaderia.R;
+
+public class Actualizar_Pedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_pedidos);
+    }
+}
