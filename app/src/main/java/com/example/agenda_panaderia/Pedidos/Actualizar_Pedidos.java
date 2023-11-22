6bb966a368062a8b57b4b63f3f3a9457ba53a730
H-Franky <carlos.hurtado1@catolica.edu.sv>
2023-11-22 15:24:09 -0600

    Actualizar_S

--- app/src/main/java/com/example/agenda_panaderia/Pedidos/Actualizar_Pedidos.java
+++ app/src/main/java/com/example/agenda_panaderia/Pedidos/Actualizar_Pedidos.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
 import com.example.agenda_panaderia.R;
 
 public class Actualizar_Pedidos extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
-    TextView Id_nota_A, Uid_Usuario_A, Fecha_registro_A, Estado_A, Fecha_A, Estado_nuevo, Estado_nuevo2;
+    TextView Id_nota_A, Uid_Usuario_A, Fecha_registro_A, Estado_A, Fecha_A, Estado_nuevo, Estado_nuevo2, Estado_E;
     EditText Titulo_A,Descripcion_A;
     Button Btn_Calendario_A;
 
@@ -33,19 +33,21 @@ public class Actualizar_Pedidos extends AppCompatActivity implements AdapterView
         Spinner_Estado();
         Spinner_Pedido();
     }
-    private void InicializarVistas(){
-        Id_nota_A =findViewById(R.id.Id_pedido_A);
-        Uid_Usuario_A=findViewById(R.id.Uid_Usuario_A);
-        Estado_A=findViewById(R.id.Estado_A);
-        Fecha_A=findViewById(R.id.Fecha_A);
-        Estado_nuevo=findViewById(R.id.Estado_nuevo);
-        Titulo_A=findViewById(R.id.Titulo_A);
-        Descripcion_A=findViewById(R.id.Descripcion_A);
-        Btn_Calendario_A=findViewById(R.id.Btn_Calendario_A);
-        Spinner_estado_2=findViewById(R.id.Spinner_estado_2);
-        Spinner_estado=findViewById(R.id.Spinner_estado);
-        Fecha_registro_A=findViewById(R.id.Fecha_registro_A);
-        Estado_nuevo2=findViewById(R.id.Estado_nuevo2);
+    private void InicializarVistas() {
+        Id_nota_A = findViewById(R.id.Id_pedido_A);
+        Uid_Usuario_A = findViewById(R.id.Uid_Usuario_A);
+        Estado_A = findViewById(R.id.Estado_A);
+        Estado_E = findViewById(R.id.Estado_E);
+        Fecha_A = findViewById(R.id.Fecha_A);
+
+        Estado_nuevo = findViewById(R.id.Estado_nuevo);
+        Titulo_A = findViewById(R.id.Titulo_A);
+        Descripcion_A = findViewById(R.id.Descripcion_A);
+        Btn_Calendario_A = findViewById(R.id.Btn_Calendario_A);
+        Spinner_estado_2 = findViewById(R.id.Spinner_estado_2);
+        Spinner_estado = findViewById(R.id.Spinner_estado);
+        Fecha_registro_A = findViewById(R.id.Fecha_registro_A);
+        Estado_nuevo2 = findViewById(R.id.Estado_nuevo2);
     }
     private void RecuperarDatos(){
         Bundle intent= getIntent().getExtras();
@@ -69,34 +71,16 @@ public class Actualizar_Pedidos extends AppCompatActivity implements AdapterView
         Fecha_A.setText(fecha_R);
         Titulo_A.setText(titulo_R);
         Descripcion_A.setText(descripcion_R);
-        Estado_nuevo2.setText(tipo_pedido_R);
-
-
-
+        Estado_E.setText(tipo_pedido_R);
     }
-    private void Spinner_Estado(){
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Estados_pedido, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner_estado.setAdapter(adapter);
-        Spinner_estado.setOnItemSelectedListener(this);
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String ESTADO_ACTUAL = Estado_A.getText().toString();
-
-
-        String Posicion_1 = adapterView.getItemAtPosition(1).toString();
-
-        String estado_seleccionado = adapterView.getItemAtPosition(i).toString();
-        Estado_nuevo.setText(estado_seleccionado);
-
-        if (ESTADO_ACTUAL.equals("Entregado")){
-            Estado_nuevo.setText(Posicion_1);
-        }
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
@@ -104,24 +88,60 @@ public class Actualizar_Pedidos extends AppCompatActivity implements AdapterView
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-    private void Spinner_Pedido(){
-
-        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.Estados_pedido2, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner_estado_2.setAdapter(adapter2);
-        Spinner_estado_2.setOnItemSelectedListener(this);
+    private void Spinner_Estado() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Estados_pedido, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner_estado.setAdapter(adapter);
+        Spinner_estado.setOnItemSelectedListener(new EstadoItemSelectedListener());
     }
 
+    private void Spinner_Pedido() {
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.Forma_pedido, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner_estado_2.setAdapter(adapter2);
+        Spinner_estado_2.setOnItemSelectedListener(new PedidoItemSelectedListener());
+    }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        String Estado_Pedido= Estado_nuevo2.getText().toString();
-        String Posicion_2= adapterView.getItemAtPosition(2).toString();
-        String estado_seleccionado2= adapterView.getItemAtPosition(position).toString();
-        Estado_nuevo2.setText(estado_seleccionado2);
-        if (Estado_Pedido.equals("Local")){
-            Estado_nuevo2.setText(Posicion_2);
+    private class EstadoItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+            String ESTADO_ACTUAL = Estado_A.getText().toString();
+            String Posicion_1 = adapterView.getItemAtPosition(1).toString();
+            String estado_seleccionado = adapterView.getItemAtPosition(i).toString();
+            Estado_nuevo.setText(estado_seleccionado);
+
+            if (ESTADO_ACTUAL.equals("Entregado")) {
+                Estado_nuevo.setText(Posicion_1);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
 
         }
     }
+
+    private class PedidoItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+            String Estado_Pedido = Estado_E.getText().toString();
+            String Posicion_2 = adapterView.getItemAtPosition(1).toString();
+            String estado_seleccionado2 = adapterView.getItemAtPosition(position).toString();
+            Estado_nuevo2.setText(estado_seleccionado2);
+
+            if (Estado_Pedido.equals("Local")) {
+                Estado_nuevo2.setText(Posicion_2);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Implementa según sea necesario
+        }
+    }
+
+
+
 }
